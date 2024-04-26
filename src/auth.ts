5d878d5bbd7253abf03df78c70322fc577fb65ff
hugocruzlfc <hugocruzlfc@gmail.com>
2024-04-26 18:41:17 +0200

    feat: adding resend email for auth

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./lib/prisma";
 import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
+import Resend from "next-auth/providers/resend";
 
 import { Adapter } from "next-auth/adapters";
 
@@ -19,5 +20,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return session;
     },
   },
-  providers: [Google, GitHub],
+  providers: [
+    Google,
+    GitHub,
+    Resend({
+      from: "no-reply@resend.dev",
+    }),
+  ],
 });
